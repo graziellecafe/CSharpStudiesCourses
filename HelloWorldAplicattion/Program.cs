f953662bf90c590bd182dedfa7fbacf499131d5c
Grazielle Café <grazielle.cafe@thoughtworks.com>
2025-12-07 17:24:41 -0300

    feat: learning the difference between CSharp and .Net

--- HelloWorldAplicattion/Program.cs
+++ HelloWorldAplicattion/Program.cs
@@ -1,5 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello everybody, let's start the studies!");
-Console.WriteLine("Chapter One: Study Variables and Types");
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello everybody, let's start the studies!");
+// Console.WriteLine("Chapter One: Study Variables and Types");
+
+namespace HelloWorldApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Is everything okay?");
+        }
+    }
+}
